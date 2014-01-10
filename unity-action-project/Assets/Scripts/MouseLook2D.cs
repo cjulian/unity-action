248f834360a1f5fc8755f5aa61f39a479dcd93dc
cjulian <carlo.julian@gmail.com>
2014-01-09 23:36:44 -0500

    MouseLook2D.cs - adding public variable to set target object's starting rotation

--- unity-action-project/Assets/Scripts/MouseLook2D.cs
+++ unity-action-project/Assets/Scripts/MouseLook2D.cs
@@ -6,6 +6,10 @@ public class MouseLook2D : MonoBehaviour
 	// The object that will be rotating to look at the mouse pointer.
 	public GameObject target;
 
+	// The angle that the target is pointing in when the scene loads.
+	// 0 corresponds to facing down the positive X axis.
+	public float targetStartAngle = 0;
+
 	// Use this for initialization
 	void Start ()
 	{
@@ -19,10 +23,9 @@ public class MouseLook2D : MonoBehaviour
 	void Update () {
 		Vector3 playerScreenPosition = Camera.main.WorldToScreenPoint(target.transform.position);
 		Vector3 direction = Input.mousePosition - playerScreenPosition;
-		float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - 90;
+		float angle = (Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg) - targetStartAngle;
 
 
 		target.transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
 	}
-	
 }
